0212c511ccf6909e523a3e425ada01739c1df595
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-01-31 23:12:15 -0500

    finished tutorial

--- coreyms_scraper.py
+++ coreyms_scraper.py
@@ -2,24 +2,23 @@ from bs4 import BeautifulSoup
 import requests
 
 source = requests.get("https://coreyms.com/").text
-soup = BeautifulSoup(source,'lxml')
+soup = BeautifulSoup(source, 'lxml')
 
-article = soup.find('article')
+for article in soup.find_all('article'):
 
-# print(article.prettify())
+    headline = article.h2.a.text
+    print(headline)
 
-# headline = article.h2.a.text
-# print(headline)
+    summary = article.find('div', class_='entry-content').p.text
+    print(summary)
 
-# summary = article.find('div', class_='entry-content').p.text
-# print(summary)
+    try:
+        vid_src = article.find("iframe", class_="youtube-player")['src']
+        vid_id = vid_src.split("/")[4]
+        vid_id = vid_id.split("?")[0]
+        yt_link = f'https://youtube.com/watch?v={vid_id}'
 
-vid_src = article.find("iframe", class_="youtube-player")['src']
-# print(vid_src)
+    except Exception as e:
+        yt_link = ""
 
-vid_id = vid_src.split("/")[4]
-vid_id = vid_id.split("?")[0]
-# print(vid_id)
-
-yt_link = f'https://youtube.com/watch?v={vid_id}'
-print(yt_link)
+    print(yt_link + "\n")
